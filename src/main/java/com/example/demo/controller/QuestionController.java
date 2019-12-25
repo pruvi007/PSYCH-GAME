2180e6973151728353849cf7aca5c1408d3a2025
Praveen Kumar <pruvi007@gmail.com>
2019-12-25 17:17:53 +0530

    update,delete in QuesController

--- src/main/java/com/example/demo/controller/QuestionController.java
+++ src/main/java/com/example/demo/controller/QuestionController.java
@@ -3,13 +3,13 @@ package com.example.demo.controller;
 import com.example.demo.model.Question;
 import com.example.demo.repository.QuestionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/dev")
@@ -36,6 +36,39 @@ public class QuestionController {
         }
         return modeQues;
 
+    }
+
+    @PostMapping( "/add-Question" )
+    public Question addQuestion( @Valid @RequestBody Question q){
+        return questionRepository.save(q);
+    }
+
+    @PutMapping( "/update-Question/{id}" )
+    public Question updateQuestion( @PathVariable( value="id" ) Long id, @Valid @RequestBody Question q) throws Exception{
+        Question old_ques = questionRepository.findById(id).orElseThrow(() -> new Exception("Something Went Worong"));
+        old_ques.setQuestionText( q.getQuestionText() );
+        old_ques.setCorrectAnswer( q.getCorrectAnswer() );
+
+        old_ques.setGameMode( q.getGameMode() );
+        old_ques.setEllenAnswers( q.getEllenAnswers() );
+        return questionRepository.save(old_ques);
+
+    }
+
+
+    @DeleteMapping( "/deleteQuestion/{id}" )
+    public ResponseEntity<?> deleteQuestion( @PathVariable( value="id" ) Long id ) throws Exception{
+        List<Question> ques_list = questionRepository.findAll();
+        for(int i=0;i<ques_list.size();i++){
+            Question foundQues = ques_list.get(i);
+            if( foundQues.getId() == id )
+            {
+                questionRepository.delete(foundQues);
+                break;
+            }
+
+        }
+        return ResponseEntity.ok().build();
     }
 
 }
